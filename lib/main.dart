d512cdb3b1a105bbb0075714015f626df37b1b7d
jasonofua <jasonofua@gmail.com>
2020-08-19 18:31:54 -0400

    making some ui changes

---
+++ lib/main.dart
@@ -0,0 +1,259 @@
+import 'dart:math';
+
+import 'package:chatapp/helper/authenticate.dart';
+import 'package:chatapp/helper/helperfunctions.dart';
+import 'package:chatapp/views/chatrooms.dart';
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+
+import 'helper/authenticate.dart';
+import 'helper/authenticate.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  static final style = TextStyle(
+    fontSize: 30,
+    fontFamily: "Billy",
+    fontWeight: FontWeight.w600,
+  );
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool userIsLoggedIn;
+  final navigatorKey = GlobalKey<NavigatorState>();
+  int page = 0;
+  LiquidController liquidController;
+  UpdateType updateType;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    liquidController = LiquidController();
+    super.initState();
+  }
+
+  final pages = [
+    Container(
+      color: Colors.pink,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/1.png',
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                "Hi",
+                style: MyApp.style,
+              ),
+              Text(
+                "It's Me",
+                style: MyApp.style,
+              ),
+              Text(
+                "Sahdeep",
+                style: MyApp.style,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+    Container(
+      color: Colors.deepPurpleAccent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/1.png',
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                "Take a",
+                style: MyApp.style,
+              ),
+              Text(
+                "look at",
+                style: MyApp.style,
+              ),
+              Text(
+                "Liquid Swipe",
+                style: MyApp.style,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+    Container(
+      color: Colors.greenAccent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/2.png',
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                "Liked?",
+                style: MyApp.style,
+              ),
+              Text(
+                "Fork!",
+                style: MyApp.style,
+              ),
+              Text(
+                "Give Star!",
+                style: MyApp.style,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+
+  ];
+
+  Widget _buildDot(int index) {
+    double selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((page ?? 0) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (2.0 - 1.0) * selectedness;
+    return new Container(
+      width: 25.0,
+      child: new Center(
+        child: new Material(
+          color: Colors.white,
+          type: MaterialType.circle,
+          child: new Container(
+            width: 8.0 * zoom,
+            height: 8.0 * zoom,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value){
+      setState(() {
+        userIsLoggedIn  = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: 'Firebase Flutter Chat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        accentColor: Color(0xff007EF4),
+        fontFamily: "OverpassRegular",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            LiquidSwipe(
+              pages: pages,
+              onPageChangeCallback: pageChangeCallback,
+              waveType: WaveType.liquidReveal,
+              liquidController: liquidController,
+              ignoreUserGestureWhileAnimating: true,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Expanded(child: SizedBox()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List<Widget>.generate(pages.length, _buildDot),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: FlatButton(
+                  onPressed: () {
+                    userIsLoggedIn != null ?  userIsLoggedIn ?  navigatorKey.currentState.pushReplacement(
+                         MaterialPageRoute(builder: (context) => ChatRoom())) :  navigatorKey.currentState.pushReplacement(
+                         MaterialPageRoute(builder: (context) => Authenticate())):
+                    navigatorKey.currentState.pushReplacement(
+                         MaterialPageRoute(builder: (context) => Authenticate()));
+                  },
+                  child:  Text("Go to Chat"),
+                  color: Colors.white.withOpacity(0.01),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: FlatButton(
+                  onPressed: () {
+                    liquidController.jumpToPage(
+                        page: liquidController.currentPage + 1);
+                  },
+                  child: Text("Next"),
+                  color: Colors.white.withOpacity(0.01),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  pageChangeCallback(int lpage) {
+    setState(() {
+      page = lpage;
+    });
+    if(page == 2){
+
+    }
+  }
+}
+
+
